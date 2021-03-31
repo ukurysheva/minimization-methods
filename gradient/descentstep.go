d90a7273b0def0cf67f8226b6615e115461c02e0
ukurysheva <kuryshevamedia@mail.ru>
2021-04-01 00:36:16 +0300

    Comments

--- gradient/descentstep.go
+++ gradient/descentstep.go
@@ -1,6 +1,6 @@
 package main
 
-// Градиентный минимизации наискорейшего спуска
+// Descent gradient minimization method
 import (
 	"fmt"
 	"math"
@@ -19,20 +19,21 @@ func Mod(x []float64) float64 {
 	return math.Cbrt(sum)
 }
 
+// Function finds the best y
 func ysearch(x []float64, grad []float64) float64 {
-	var a float64 = 0 // Начало отрезка
-	var b float64 = 1 // Конец отрезка
+	var a float64 = 0 // Begin point
+	var b float64 = 1 // End point
 	var Eps float64 = 0.0001
-	var N float64 = (b - a) / Eps // Количество отрезков
-	var Fmin float64 = Fn(x)      // Минимум функции
-	var ymin float64 = 0          // Гамма минимальная
-	var i float64 = (b - a) / N   // Расстояние между точками
+	var N float64 = (b - a) / Eps
+	var Fmin float64 = Fn(x)    // Min value of function
+	var ymin float64 = 0        // Y min
+	var i float64 = (b - a) / N // Step
 	var x11 float64
 	var x12 float64
 	for y := a + i; y <= b; y += i {
 		x11 = x[0] - y*grad[0]
 		x12 = x[1] - y*grad[1]
-		x2 := []float64{x11, x12} // Начальная точка
+		x2 := []float64{x11, x12}
 		if Fn(x2) <= Fmin {
 			Fmin = Fn(x2)
 			ymin = y
@@ -41,18 +42,20 @@ func ysearch(x []float64, grad []float64) float64 {
 
 	return ymin
 }
+
+// Function finding min value
 func findMin(M float64, y float64, Eps1 float64, Eps2 float64) (float64, []float64, float64, float64) {
 
-	var x01 float64 = 7      // Координата начальной точки
-	var x02 float64 = 7      // Координата начальной точки
-	x := []float64{x01, x02} // Начальная точка
-	var k float64 = 0        // Счетчик
-	var kf float64 = 0       // Счетчик вычисления функций
+	var x01 float64 = 7 // Begin point - x
+	var x02 float64 = 7 // Begin point - y
+	x := []float64{x01, x02}
+	var k float64 = 0  // Iterator
+	var kf float64 = 0 // Iterator of function calculation
 	x2 := []float64{}
 
-	dst := make([]float64, 2) // Массив, в который будет помещен результат
+	dst := make([]float64, 2) // The result array
 
-	// Вычисляем градиент в начальной точке
+	// Find gradient in the beginning point
 	res := fd.Gradient(dst, Fn, x, &fd.Settings{
 		Step: 1e-3,
 	})
@@ -64,14 +67,17 @@ func findMin(M float64, y float64, Eps1 float64, Eps2 float64) (float64, []float
 			})
 
 			y = ysearch(x, res)
+
+			// New point x
 			var x11 float64 = x[0] - y*res[0]
 			var x12 float64 = x[1] - y*res[1]
-			x2 = []float64{x11, x12} // Начальная точка
+			x2 = []float64{x11, x12}
 
 			var x1diff float64 = x2[0] - x[0]
 			var x2diff float64 = x2[1] - x[1]
-			xdiff := []float64{x1diff, x2diff} // Начальная точка
+			xdiff := []float64{x1diff, x2diff} // Find norm
 			kf += 2
+
 			if Mod(xdiff) <= Eps2 && math.Abs(Fn(x2)-Fn(x)) <= Eps2 {
 				x = x2
 				break
@@ -86,20 +92,18 @@ func findMin(M float64, y float64, Eps1 float64, Eps2 float64) (float64, []float
 		} else {
 			break
 		}
-
 	}
-
 	return Fn(x), x, k, kf
 }
 func main() {
 
-	var M float64 = 100 // Чисто итераций
-	var y float64 = 1   // Гамма
+	var M float64 = 100 // The maximum count if iterations
+	var y float64 = 1
 	var Eps1 float64 = 0.1
 	Fmin, x, k, kf := findMin(M, y, Eps1, Eps1)
 	fmt.Println("EPS: ", Eps1)
-	fmt.Println("Минимум функции: ", Fmin)
-	fmt.Println("Точка минимума: ", x)
-	fmt.Println("Количество шагов: ", k)
-	fmt.Println("Количество вычислений функции: ", kf, "\n")
+	fmt.Println("Min value of F: ", Fmin)
+	fmt.Println("Min point: ", x)
+	fmt.Println("Iterations: ", k)
+	fmt.Println("Iterations of F calculations: ", kf, "\n")
 }
--- gradient/stepsize.go
+++ gradient/stepsize.go
@@ -1,6 +1,6 @@
 package main
 
-// Градиентный минимизации - с постоянным шагом
+// Gradient minimization method with constant step
 import (
 	"fmt"
 	"math"
@@ -19,18 +19,19 @@ func Mod(x []float64) float64 {
 	return math.Cbrt(sum)
 }
 
+// Function finding min value
 func findMin(M float64, ylocal float64, Eps1 float64, Eps2 float64) (float64, []float64, float64, float64) {
 
-	var x01 float64 = 7      // Координата начальной точки
-	var x02 float64 = 7      // Координата начальной точки
-	x := []float64{x01, x02} // Начальная точка
+	var x01 float64 = 7 // Begin point - y
+	var x02 float64 = 7 // Begin point - y
+	x := []float64{x01, x02}
 	var xlocal []float64 = x
-	var k float64 = 0  // Счетчик итераций
-	var kf float64 = 0 // Счетчик вычисления функций
+	var k float64 = 0  // Iterator of steps
+	var kf float64 = 0 // Iterator of function calculation
 	x2 := []float64{}
-	dst := make([]float64, 2) // Массив, в который будет помещен результат
+	dst := make([]float64, 2) // The result array
 
-	// Вычисляем градиент в начальной точке
+	// Find gradient in the beginning point
 	res := fd.Gradient(dst, Fn, xlocal, &fd.Settings{
 		Step: 1e-3,
 	})
@@ -41,23 +42,26 @@ func findMin(M float64, ylocal float64, Eps1 float64, Eps2 float64) (float64, []
 				Step: 1e-3,
 			})
 
+			// Check if y is okay for us
 			var yIsFine bool = false
 			for !yIsFine {
 				var x11 float64 = xlocal[0] - ylocal*res[0]
 				var x12 float64 = xlocal[1] - ylocal*res[1]
-				x2 = []float64{x11, x12} // Новая точка
+				x2 = []float64{x11, x12}
+
+				// If we overstep - change direction by changing y
 				if Fn(x2)-Fn(xlocal) < 0 {
 					yIsFine = true
 				} else {
 					ylocal = ylocal / 2
 				}
-				// Увеличиваем счетчик вычисления функции
 				kf += 2
 			}
-
+			// New point x
 			var x1diff float64 = x2[0] - xlocal[0]
 			var x2diff float64 = x2[1] - xlocal[1]
-			xdiff := []float64{x1diff, x2diff} // Начальная точка
+			xdiff := []float64{x1diff, x2diff}
+
 			if Mod(xdiff) <= Eps2 && math.Abs(Fn(x2)-Fn(xlocal)) <= Eps2 {
 				xlocal = x2
 				break
@@ -78,15 +82,14 @@ func findMin(M float64, ylocal float64, Eps1 float64, Eps2 float64) (float64, []
 }
 func main() {
 
-	var M float64 = 100 // Максимальное число итераций
-
-	var y float64 = 1 // Лямбда
+	var M float64 = 100 // The maximum count if iterations
+	var y float64 = 1
 	var Eps1 float64 = 0.1
 
 	Fmin, x, k, kf := findMin(M, y, Eps1, Eps1)
 	fmt.Println("EPS: ", Eps1)
-	fmt.Println("Минимум функции: ", Fmin)
-	fmt.Println("Точка минимума: ", x)
-	fmt.Println("Количество шагов: ", k)
-	fmt.Println("Количество вычислений функции: ", kf, "\n")
+	fmt.Println("Min value of F: ", Fmin)
+	fmt.Println("Min point: ", x)
+	fmt.Println("Iterations: ", k)
+	fmt.Println("Iterations of F calculations: ", kf, "\n")
 }
